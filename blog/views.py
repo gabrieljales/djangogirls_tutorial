2aac392cd5bf21b85d7911f9483134274cf0450e
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2020-10-03 11:00:21 -0300

    Create the 'posts' QuerySet

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # posts is a QuerySet
+    posts = Post.objects.filter(published_date__lte=timezone.now()) # Filter by publication date
+    return render(request, 'blog/post_list.html', {'posts':posts})
