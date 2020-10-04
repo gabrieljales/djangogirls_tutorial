39a38e6922b1a436bd3f6757b3d0a3854c008436
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2020-10-03 21:42:47 -0300

    Viewss for create and edit posts added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     # posts is a QuerySet
@@ -10,3 +11,30 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request): # Create a new post
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False) # We don't want to save the post right now, because we want to add the author of the post first!
+            post.author = request.user # Post author
+            post.published_date = timezone.now()
+            post.save() # Save the post (with the author)
+            return redirect('post_detail', pk=post.pk)
+    else:
+         form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk): # Edit a post
+     post = get_object_or_404(Post, pk=pk) # Select the model to be edited
+     if request.method == "POST":
+         form = PostForm(request.POST, instance=post)
+         if form.is_valid():
+             post = form.save(commit=False)
+             post.author = request.user
+             post.published_date = timezone.now()
+             post.save()
+             return redirect('post_detail', pk=post.pk)
+     else:
+         form = PostForm(instance=post)
+     return render(request, 'blog/post_edit.html', {'form': form})
